5366706f8c16399d6106c6b235fee406830bae8a
Shehab Tarek <shehabsoft94@gmail.com>
2018-06-10 06:58:21 +0200

    Conf

---
+++ src/main/java/com/user/ActiviationApplication.java
@@ -0,0 +1,15 @@
+package com.user;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiviationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiviationApplication.class, args);
+		
+		 System.out.println("Final");
+	}
+}
